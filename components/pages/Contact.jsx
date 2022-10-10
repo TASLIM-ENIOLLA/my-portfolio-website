bdd1b022cafedd2c9bcd723bb66336813c84e737
TASLIM-ENIOLLA <61813017+TASLIM-ENIOLLA@users.noreply.github.com>
2022-10-10 22:50:18 +0100

    Update Contact.jsx

--- components/pages/Contact.jsx
+++ components/pages/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact(){
                         <div className = 'text-center'>
                             <p><span className = 'ms theme-color fo-s-15'>04. What's next?</span></p>
                             <p className = 'bold fa-4x letter-spacing-1 heavy text-capitalize mb-3'>get in touch</p>
-                            <p className = 'fo-s-15 text-muted px-0'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p className = 'fo-s-15 text-muted px-0'>You're only a few clicks from sending me a message. I'll be happy to hear from you!</p>
                             <div className = 'pt-5'>
                                 <a href = 'mailto://taslim.eniolla@gmail.com' className = 'fo-s-16 theme-color ms d-inline-block text-capitalize theme-border px-4 py-3 rounded'>say hello</a>
                             </div>
